98f554ac538821a6b9364b423cadd572e3801f5c
Gakkiii <h1300704118@qq.com>
2021-02-03 22:39:24 +0800

    选择排序

--- src/Sort.java
+++ src/Sort.java
@@ -6,6 +6,10 @@ public class Sort {
     然后将第二位和第三位进行比较，直到将第二大的数字放在第二位。
      */
     public static void main(String[] args) {
+        selectSort();
+    }
+
+    public static void selectSort(){
         int a[] = new int[]{1,3,5,3,2,1,5,7,8,99,3,4,5,};
         for(int i=0;i<a.length;i++){
             for(int j=i+1;j<a.length;j++){
